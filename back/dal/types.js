8d64e2549f4bd7c4d2c37675867f06d4d0b08590
ZK <k.zubkov@gamil.com>
2020-07-20 11:52:37 +0300

    36 -> 24 hours pay time coditions reduce

--- back/dal/types.js
+++ back/dal/types.js
@@ -90,10 +90,10 @@ Game.prototype.freeSlotExists = function (slotType) {
 
 Game.prototype.waiterReservationTTL = function () {
   if (this.isPrepay()) {
-    if (this.hoursToGameBegin() > 36) {
+    if (this.hoursToGameBegin() > 24) {
       return Date.now() + 12*60*60*1000;
     }
-    return Date.now() + 4*60*60*1000;
+    return Date.now() + 2*60*60*1000;
   }
   return 0;
 };
